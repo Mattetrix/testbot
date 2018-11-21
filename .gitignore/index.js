72d62fbea152d3271961ff97575971e0bdd06213
Mattetrix <45236460+Mattetrix@users.noreply.github.com>
2018-11-21 21:20:38 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,21 +1,13 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const prefix = ("?");
-
-
-const ban = require('./ban');
+const prefix = ("URSS.");
 
 
 
-bot.on('message', function (message){
-    if (ban.match(message)){
-        return ban.action(message)
-    }
-});    
 
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
-    bot.user.setActivity('En développement').catch(console.error)
+    bot.user.setActivity('Drinking Vodka').catch(console.error)
 });
     
 
@@ -65,19 +57,24 @@ bot.on('message', message => {
   }
 
 })
-    
+  bot.on('message', msg => {
+      if (content.startsWith(prefix + "time")) {
+        let daysofweek = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi" ]
+        msg.channel.send(daysofweek[(new Date ).getDay()])
+      }
+  bot.on('message', msg => {
+      if (content.startsWith(prefix + "roulette russe")) {
+        msg.channel.send("Et c'est ..." + Math.floor(Math.random() * 6 + 1)
+    if (msg.content == "Et c'est ... 6" )
+    else guildMember.kick
+    guildMember.createDM("https://discord.gg/YYgYZDn")
+  }
 
 
 bot.on('message', msg => {
-    if (msg.content === "bonjour"){
-        msg.reply("Heureux de te revoir parmis nous.")
-    }
-    if (msg.content.match(/salut/i)) {
-            msg.reply('Je suis d\'accord avec toi.')
-    }
     if (msg.content === prefix + "site"){
-        msg.channel.send("https://aezioxshop.jimdofree.com")
-        console.log("Une personne a demandé pour aller sur ton site.")
+        msg.channel.send("https://fr.wikipedia.org/wiki/Union_des_républiques_socialistes_soviétiques")
+        console.log("Un camarade en plus")
     }
 
 });
